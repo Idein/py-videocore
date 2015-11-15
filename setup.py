da8fec4bf2c7d005c8a38ae306a5af8d8547daf0
Koichi NAKAMURA <koichi@idein.jp>
2015-11-15 23:22:22 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = 'py-videocore',
+      version = '0.1',
+      description = 'Python library for GPGPU programming on Raspberry Pi',
+      author = 'Koichi Nakamura',
+      author_email = 'koichi@idein.jp',
+      url = 'https://github.com/nineties/py-videocore',
+      packages = ['videocore']
+      )
