fe49e98b0f547b8be79d41422a8f9f1fe83ab9cc
Koichi NAKAMURA <koichi@idein.jp>
2015-11-15 22:52:59 +0900

    Changed contents of examples/hello_world.py

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -2,28 +2,45 @@ from videocore.assembler import qpucode
 from videocore.driver import Driver
 import numpy as np
 
-NUM_THREADS = 16
-
 @qpucode
-def helloworld(asm):
+def hello_world(asm):
+    # Load two vectors of length 16 from the host memory (address=uniforms[0]) to VPM
+    setup_dma_load(nrows = 2)
+    start_dma_load(uniform)
+    wait_dma_load()
+
+    # Setup VPM read/write operaitons
+    setup_vpm_read(nrows = 2)
     setup_vpm_write()
-    setup_dma_store(nrows = 1)
 
-    # Add uniform[:,0] and SIMD element number
-    iadd(vpm, uniform, element_number)
+    mov(r0, vpm) # vector a
+    mov(r1, vpm) # vector b
+    fadd(vpm, r0, r1)
 
-    # Write results back to host.
+    # Store the result vector from VPM to the host memory (address=uniforms[1])
+    setup_dma_store(nrows = 2)
     start_dma_store(uniform)
     wait_dma_store()
 
+    # Finish the thread
     exit()
 
 with Driver() as drv:
-    prog     = drv.program(helloworld)
-    result   = drv.array((NUM_THREADS, 16), 'uint32')
-    uniforms = drv.array((NUM_THREADS,  2), 'uint32')
+    # Input vectors
+    a    = np.random.random(16).astype('f4')
+    b    = np.random.random(16).astype('f4')
 
-    uniforms[:, 0] = np.arange(0, NUM_THREADS*16, 16)
-    uniforms[:, 1] = result.addresses[:, 0]
-    prog(NUM_THREADS, uniforms, timeout=1000)
-    print(result)
+    # Allocate vectors in shared memory for DMA transfer
+    inp  = drv.copy(np.r_[a, b])
+    out  = drv.alloc(16, 'f4')
+
+    # Run the program
+    drv.execute(
+            num_threads = 1,
+            program     = drv.program(hello_world),
+            uniforms    = [inp.address, out.address]
+            )
+
+    print(a)
+    print(b)
+    print(out)
