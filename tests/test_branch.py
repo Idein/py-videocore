4181f1bbb955cffe60c0da2f9fe3b2e94a6f9a26
Koichi NAKAMURA <koichi@idein.jp>
2015-11-21 12:43:32 +0900

    Add many testcases for jump.

--- tests/test_branch.py
+++ tests/test_branch.py
@@ -1,5 +1,6 @@
 'Test of Branch instruction'
 
+import inspect
 import numpy as np
 
 from videocore.assembler import qpu
@@ -18,7 +19,7 @@ def boilerplate(asm, f, nout):
 
 def run_code(code, nout):
     with Driver() as drv:
-        X = drv.alloc((nout, 16), 'uint32')
+        X = drv.alloc((nout, 16), 'int32')
         drv.execute(
                 num_threads=1,
                 program=drv.program(boilerplate, code, nout),
@@ -26,18 +27,150 @@ def run_code(code, nout):
                 )
         return np.copy(X)
 
+ASSERT_OK = 1
+ASSERT_NG = -1
+
+LABEL_COUNTER = 0
+def fresh_label():
+    global LABEL_COUNTER
+    LABEL_COUNTER += 1
+    return '_' + str(LABEL_COUNTER)
+
 @qpu
-def jmp_always(asm):
-    mov(r0, 1)
-    jmp(L._1)
-    nop()
+def assert_jump(asm, do_jump, br_insn):
+    label = fresh_label()
+
+    mov(ra0, [ASSERT_NG, ASSERT_OK][do_jump])
+
+    br_insn(L[label])
+    nop()   # delay slots
     nop()
     nop()
 
-    mov(r0, 2)
-    L._1
-    mov(vpm, r0)
+    mov(ra0, [ASSERT_OK, ASSERT_NG][do_jump])
+    nop()
 
-def test_jmp_always():
-    X = run_code(jmp_always, 1)
-    assert np.all(X == 1)
+    L[label]
+
+    mov(vpm, ra0)
+
+def jmp_code_1(asm):
+    ldi(r0, [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+    ldi(r1, [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+    ldi(r2, [1,1,1,0,1,1,0,1,0,1,1,1,1,1,1,1])
+    ldi(r3, [1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1])
+
+    assert_jump(asm, 1, jmp)
+
+    isub(null, r0, r1)  # no Z, no N, no C
+    assert_jump(asm, 0, jzs)
+    assert_jump(asm, 1, jzc)
+    assert_jump(asm, 0, jzs_any)
+    assert_jump(asm, 1, jzc_any)
+    assert_jump(asm, 0, jns)
+    assert_jump(asm, 1, jnc)
+    assert_jump(asm, 0, jns_any)
+    assert_jump(asm, 1, jnc_any)
+    assert_jump(asm, 0, jcs)
+    assert_jump(asm, 1, jcc)
+    assert_jump(asm, 0, jcs_any)
+    assert_jump(asm, 1, jcc_any)
+
+    isub(null, r0, r0)  # all Z, no N, no C
+    assert_jump(asm, 1, jzs)
+    assert_jump(asm, 0, jzc)
+    assert_jump(asm, 1, jzs_any)
+    assert_jump(asm, 0, jzc_any)
+    assert_jump(asm, 0, jns)
+    assert_jump(asm, 1, jnc)
+    assert_jump(asm, 0, jns_any)
+    assert_jump(asm, 1, jnc_any)
+    assert_jump(asm, 0, jcs)
+    assert_jump(asm, 1, jcc)
+    assert_jump(asm, 0, jcs_any)
+    assert_jump(asm, 1, jcc_any)
+
+    isub(null, r1, r0)  # no Z, all N, all C
+    assert_jump(asm, 0, jzs)
+    assert_jump(asm, 1, jzc)
+    assert_jump(asm, 0, jzs_any)
+    assert_jump(asm, 1, jzc_any)
+    assert_jump(asm, 1, jns)
+    assert_jump(asm, 0, jnc)
+    assert_jump(asm, 1, jns_any)
+    assert_jump(asm, 0, jnc_any)
+    assert_jump(asm, 1, jcs)
+    assert_jump(asm, 0, jcc)
+    assert_jump(asm, 1, jcs_any)
+    assert_jump(asm, 0, jcc_any)
+
+    isub(null, r0, r2)  # some Z, no N, no C
+    assert_jump(asm, 0, jzs)
+    assert_jump(asm, 0, jzc)
+    assert_jump(asm, 1, jzs_any)
+    assert_jump(asm, 1, jzc_any)
+    assert_jump(asm, 0, jns)
+    assert_jump(asm, 1, jnc)
+    assert_jump(asm, 0, jns_any)
+    assert_jump(asm, 1, jnc_any)
+    assert_jump(asm, 0, jcs)
+    assert_jump(asm, 1, jcc)
+    assert_jump(asm, 0, jcs_any)
+    assert_jump(asm, 1, jcc_any)
+
+    isub(null, r2, r3)  # some Z, some N, some C
+    assert_jump(asm, 0, jzs)
+    assert_jump(asm, 0, jzc)
+    assert_jump(asm, 1, jzs_any)
+    assert_jump(asm, 1, jzc_any)
+    assert_jump(asm, 0, jns)
+    assert_jump(asm, 0, jnc)
+    assert_jump(asm, 1, jns_any)
+    assert_jump(asm, 1, jnc_any)
+    assert_jump(asm, 0, jcs)
+    assert_jump(asm, 0, jcc)
+    assert_jump(asm, 1, jcs_any)
+    assert_jump(asm, 1, jcc_any)
+
+def jmp_code_2(asm):
+    ldi(r1, 0xffffffff)
+    iadd(null, r1, 1)       # all Z, no N, all C
+    assert_jump(asm, 1, jzs)
+    assert_jump(asm, 0, jzc)
+    assert_jump(asm, 1, jzs_any)
+    assert_jump(asm, 0, jzc_any)
+    assert_jump(asm, 0, jns)
+    assert_jump(asm, 1, jnc)
+    assert_jump(asm, 0, jns_any)
+    assert_jump(asm, 1, jnc_any)
+    assert_jump(asm, 1, jcs)
+    assert_jump(asm, 0, jcc)
+    assert_jump(asm, 1, jcs_any)
+    assert_jump(asm, 0, jcc_any)
+
+    ldi(r2, [1,1,1,0,1,1,0,1,0,1,1,1,1,1,1,1])
+    iadd(null, r1, r2)      # some Z, some N, some C
+    assert_jump(asm, 0, jzs)
+    assert_jump(asm, 0, jzc)
+    assert_jump(asm, 1, jzs_any)
+    assert_jump(asm, 1, jzc_any)
+    assert_jump(asm, 0, jns)
+    assert_jump(asm, 0, jnc)
+    assert_jump(asm, 1, jns_any)
+    assert_jump(asm, 1, jnc_any)
+    assert_jump(asm, 0, jcs)
+    assert_jump(asm, 0, jcc)
+    assert_jump(asm, 1, jcs_any)
+    assert_jump(asm, 1, jcc_any)
+
+def test_jump():
+    nout = inspect.getsource(jmp_code_1).count('assert_jump')
+    X = run_code(qpu(jmp_code_1), nout)
+    assert np.all(X == ASSERT_OK)
+
+    nout = inspect.getsource(jmp_code_2).count('assert_jump')
+    X = run_code(qpu(jmp_code_2), nout)
+    for i in range(nout):
+        if not all(X[i] == ASSERT_OK):
+            print i
+        assert all(X[i] == ASSERT_OK)
