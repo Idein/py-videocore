980abdd39e6832eaed1c208ec46bf53f2a3edc3c
Koichi NAKAMURA <koichi@idein.jp>
2015-11-23 00:00:16 +0900

    Update examples/mailbox.py

--- examples/mailbox.py
+++ examples/mailbox.py
@@ -1,11 +1,7 @@
-from pyvc.mailbox import MailBox
+from videocore.mailbox import MailBox
 
 with MailBox() as mb:
-    firmware_rev   = mb.get_firmware_revision()
-    board_model    = mb.get_board_model()
-    board_revision = mb.get_board_revision()
-    board_serial   = mb.get_board_serial()
-    print('firmware revision: %x' % firmware_rev)
-    print('board model: %x' % board_model)
-    print('board revision: %x' % board_revision)
-    print('board serial: %016x' % board_serial)
+    print('firmware revision: %x' % mb.get_firmware_revision())
+    print('board model: %x' % mb.get_board_model())
+    print('board revision: %x' % mb.get_board_revision())
+    print('board serial: %016x' % mb.get_board_serial())
