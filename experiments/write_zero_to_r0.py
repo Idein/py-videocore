c01d3f11a49aecc3b38deff182489046eaeec414
Koichi NAKAMURA <koichi@idein.jp>
2015-11-22 19:29:54 +0900

    Add comments and removed unnecessary module imports.

--- experiments/write_zero_to_r0.py
+++ experiments/write_zero_to_r0.py
@@ -1,41 +1,38 @@
 'Undocumented strange behavior of storing data to r0'
 
-import numpy as np
-from textwrap import dedent
-
 from videocore.assembler import qpu
-from videocore.driver import Driver, DriverError
+from videocore.driver import Driver
 
 @qpu
-def case1(asm):
-    ldi(r0, 0)
+def case1(asm)
+    ldi(r0, 0)  # halt
     exit()
 
 @qpu
 def case2(asm):
-    mov(r0, 1)
+    mov(r0, 1)  # does not halt
     exit()
 
 @qpu
 def case3(asm):
-    mov(r1, 0)
+    mov(r1, 0)  # does not halt
     exit()
 
 @qpu
 def case4(asm):
-    ldi(r0, [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+    ldi(r0, [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])  # halt
     exit()
 
 @qpu
 def case5(asm):
-    ldi(r0, [1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+    ldi(r0, [1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1])  # does not halt
     exit()
 
 @qpu
 def case6(asm):
     ldi(r1, 1)
     ldi(r2, 1)
-    isub(r0, r1, r2)
+    isub(r0, r1, r2)    # halt
     exit()
 
 def test(prog):
