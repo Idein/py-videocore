cff6bd92e4445e8e4d7d57eed9669fb0ad311606
Koichi NAKAMURA <koichi@idein.jp>
2015-12-09 09:44:55 +0900

    Add new test.

--- tests/test_dma.py
+++ tests/test_dma.py
@@ -8,7 +8,7 @@ from videocore.driver import Driver
 #=================================== 32 bit ===================================
 
 @qpu
-def horizontal_32bit_full(asm):
+def horizontal_32bit_load(asm):
     mov(ra0, uniform)
     ldi(rb0, 4*16*16)
     for i in range(4):
@@ -22,7 +22,7 @@ def horizontal_32bit_full(asm):
     wait_dma_store()
     exit()
 
-def test_horizontal_32bit_full():
+def test_horizontal_32bit_load():
     with Driver() as drv:
         X = drv.alloc((64, 16), dtype='uint32')
         X[:] = np.arange(64*16).reshape(64, 16).astype('uint32')
@@ -30,10 +30,74 @@ def test_horizontal_32bit_full():
 
         drv.execute(
                 n_threads=1,
-                program=drv.program(horizontal_32bit_full),
+                program=drv.program(horizontal_32bit_load),
                 uniforms=[X.address, Y.address]
                 )
         assert np.all(X == Y)
+
+@qpu
+def vertical_32bit_load(asm):
+    mov(ra0, uniform)
+    ldi(rb0, 4*16*4)
+    for x in range(16):
+        setup_dma_load(Y=0, X=x, mode='32bit vertical', nrows=4, vpitch=0)
+        start_dma_load(ra0)
+        iadd(ra0, ra0, rb0)
+    wait_dma_load()
+
+    setup_dma_store(mode='32bit horizontal', nrows=64)
+    start_dma_store(uniform)
+    wait_dma_store()
+    exit()
+
+def test_vertical_32bit_load():
+    with Driver() as drv:
+        X = drv.alloc((16, 64), dtype='uint32')
+        X[:] = np.arange(16*64).reshape(16, 64).astype('uint32')
+        Y = drv.alloc((64, 16), dtype='uint32')
+
+        drv.execute(
+                n_threads=1,
+                program=drv.program(vertical_32bit_load),
+                uniforms=[X.address, Y.address]
+                )
+
+        assert np.all(X == Y.T)
+
+@qpu
+def vertical_32bit_store(asm):
+    mov(ra0, uniform)
+    ldi(rb0, 4*16*16)
+    for i in range(4):
+        setup_dma_load(Y=16*i, mode='32bit horizontal', nrows=16)
+        start_dma_load(ra0)
+        iadd(ra0, ra0, rb0)
+    wait_dma_load()
+
+    mov(ra0, uniform)
+    ldi(rb0, 4*16)
+    for x in range(16):
+        for y in range(0, 64, 16):
+            setup_dma_store(Y=y, X=x, mode='32bit vertical', nrows=1, ncols=16)
+            start_dma_store(ra0)
+            iadd(ra0, ra0, rb0)
+    wait_dma_store()
+    exit()
+
+def test_vertical_32bit_store():
+    with Driver() as drv:
+        X = drv.alloc((64, 16), dtype='uint32')
+        X[:] = np.arange(64*16).reshape(64, 16).astype('uint32')
+        Y = drv.alloc((16, 64), dtype='uint32')
+        Y[:] = 0
+
+        drv.execute(
+                n_threads=1,
+                program=drv.program(vertical_32bit_store),
+                uniforms=[X.address, Y.address]
+                )
+
+        assert np.all(X == Y.T)
 
 @qpu
 def horizontal_32bit_partial(asm):
