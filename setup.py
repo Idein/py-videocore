0abc33b11137e94ec59ec3a585eb7fcf5c881299
Koichi NAKAMURA <koichi@idein.jp>
2015-12-28 18:36:13 +0900

    Remove unnecessary spaces.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from videocore import __version__
 from distutils.core import setup
 
-setup(name = 'py-videocore',
-      version = __version__,
-      description = 'Python library for GPGPU programming on Raspberry Pi',
-      author = 'Koichi Nakamura',
-      author_email = 'koichi@idein.jp',
-      url = 'https://github.com/nineties/py-videocore',
-      packages = ['videocore']
+setup(name='py-videocore',
+      version=__version__,
+      description='Python library for GPGPU programming on Raspberry Pi',
+      author='Koichi Nakamura',
+      author_email='koichi@idein.jp',
+      url='https://github.com/nineties/py-videocore',
+      packages=['videocore']
       )
