53caa8d519ee3024f67a68b5b90bc487b123d4ef
long-long-float <niinikazuki@yahoo.co.jp>
2019-06-14 21:12:24 +0900

    Add a test of raw_program

--- tests/test_raw.py
+++ tests/test_raw.py
@@ -3,6 +3,7 @@
 import io
 import numpy as np
 from random import getrandbits
+from ctypes import create_string_buffer
 
 from videocore.assembler import qpu, print_qhex
 from videocore.driver import Driver
@@ -11,7 +12,17 @@ from videocore.driver import Driver
 def raw_hex(asm):
     raw(0xDEADBEEF, 0xFEEDFACE)
 
+@qpu
+def raw_program_hex(asm):
+    data = b'\xEF\xBE\xAD\xDE\xCE\xFA\xED\xFE\xBE\xBE\xFE\xCA\x0D\xF0\xDD\xBA'
+    raw_program(create_string_buffer(data, len(data)), len(data))
+
 def test_raw_hex():
     f = io.StringIO()
     print_qhex(raw_hex, file = f)
     assert f.getvalue().rstrip() == '0xDEADBEEF, 0xFEEDFACE,'
+
+def test_raw_program():
+    f = io.StringIO()
+    print_qhex(raw_program_hex, file = f)
+    assert f.getvalue().rstrip() == '0xDEADBEEF, 0xFEEDFACE,\n0xCAFEBEBE, 0xBADDF00D,'
